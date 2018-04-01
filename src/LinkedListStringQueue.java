1b2a22a034a8705a416dc8c360905f17271d9407
Parker Smith <parker@parkersmith.us>
2018-03-31 21:47:43 -0600

    Add functionality and tests

--- src/LinkedListStringQueue.java
+++ src/LinkedListStringQueue.java
@@ -2,8 +2,43 @@
  * Created by parker on 3/31/18.
  */
 public class LinkedListStringQueue {
+
+    private Node last = null;
+    private Node first = null;
+
+    private class Node
+    {
+        String item;
+        Node next;
+    }
+
+    public String dequeue()
+    {
+        String item = first.item;
+        first = first.next;
+        return item;
+    }
+
+    public void enqueue(String item)
+    {
+        Node node = new Node();
+        node.item = item;
+        node.next = null;
+
+        if (first == null)
+        {
+            first = node;
+        }
+        else
+        {
+            last.next = node;
+        }
+
+        last = node;
+    }
+
     public boolean isEmpty()
     {
-        return true;
+        return first == null;
     }
 }
--- test/LinkedListStringQueueTest.java
+++ test/LinkedListStringQueueTest.java
@@ -1,14 +1,43 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 /**
  * Created by parker on 3/31/18.
  */
 public class LinkedListStringQueueTest {
+    private LinkedListStringQueue sq;
+
+    @BeforeEach
+    void initEach()
+    {
+        sq = new LinkedListStringQueue();
+    }
+
     @Test
     void is_empty()
     {
-        LinkedListStringQueue sq = new LinkedListStringQueue();
         Assertions.assertTrue(sq.isEmpty());
     }
+
+    @Test
+    void not_empty()
+    {
+        sq.enqueue("test");
+        Assertions.assertFalse(sq.isEmpty());
+    }
+
+    @Test
+    void enqueue_dequeue()
+    {
+        sq.enqueue("test");
+        sq.enqueue("test2");
+        sq.enqueue("test3");
+        sq.enqueue("test4");
+        Assertions.assertEquals("test", sq.dequeue());
+        Assertions.assertEquals("test2", sq.dequeue());
+        Assertions.assertEquals("test3", sq.dequeue());
+        Assertions.assertEquals("test4", sq.dequeue());
+    }
+
 }
