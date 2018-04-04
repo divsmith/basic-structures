125c26f39ed57b3e4b6288c72408efd8eb1a3bf9
Parker Smith <parker@parkersmith.us>
2018-04-04 00:01:49 -0600

    Extend iterable from interfaces

--- src/QueueInterface.java
+++ src/QueueInterface.java
@@ -1,7 +1,7 @@
 /**
  * Created by parker on 4/2/18.
  */
-public interface QueueInterface<Item> {
+public interface QueueInterface<Item> extends Iterable<Item> {
     public void enqueue(Item item);
     public Item dequeue();
 }
--- src/StackInterface.java
+++ src/StackInterface.java
@@ -1,7 +1,7 @@
 /**
  * Created by parker on 3/31/18.
  */
-public interface StackInterface<Item> {
+public interface StackInterface<Item> extends Iterable<Item> {
     public void push(Item item);
     public Item pop();
     public boolean isEmpty();
