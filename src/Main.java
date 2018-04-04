2bc09e1fa12ca56ca03a9cd37b2c6d334b03daba
Parker Smith <parker@parkersmith.us>
2018-04-03 23:43:02 -0600

    Remove driver code

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,5 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-
-        StackArray<Integer> sa = new StackArray<>();
-        sa.push(1);
-        sa.push(2);
-        sa.push(3);
-        sa.push(4);
-
-        for (int i : sa)
-        {
-            System.out.println(i);
-        }
     }
 }
